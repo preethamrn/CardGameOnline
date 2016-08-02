681812610235ee0fa4d31b22a50508dbd8dc2719
Preetham Reddy Narayanareddy <preetham.in.1997@gmail.com>
2016-08-02 15:41:31 -0700

    Gets list of games (todo: selection and switch to new scene with resources loaded)

---
+++ Assets/scripts/ListGames.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class ListGames : MonoBehaviour
+{
+    List<string> directories;
+    
+    void Start()
+    {
+        directories = GetDirectories(Application.dataPath+"/Games", "*");
+    }
+
+    void OnGUI()
+    {
+        int i = 0;
+        foreach (string dir in directories) {
+            GUI.Label(new Rect(0, 10*i, 1000, 100), dir);
+            i++;
+        }
+    }
+
+    private static List<string> GetDirectories(string path, string searchPattern)
+    {
+        try
+        {
+            return Directory.GetDirectories(path, searchPattern).ToList();
+        }
+        catch (UnauthorizedAccessException)
+        {
+            return new List<string>();
+        }
+    }
+}
