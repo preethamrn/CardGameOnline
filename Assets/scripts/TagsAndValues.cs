99ceca32ee1fb6abc753b8574d3c0f5e3b63a7ba
Allen Zhou <Zhouball@gmail.com>
2016-10-10 14:30:35 -0700

    Change name of file

--- Assets/scripts/TagsAndValues.cs
+++ Assets/scripts/TagsAndValues.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TagsComponent : MonoBehaviour {
+public class TagsAndValues : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
