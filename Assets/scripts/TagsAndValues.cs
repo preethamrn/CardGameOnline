143199a9ea7f23a573479a8b8c28474ded5badae
Allen Zhou <Zhouball@gmail.com>
2016-10-10 15:01:53 -0700

    Working on tags and values

--- Assets/scripts/TagsAndValues.cs
+++ Assets/scripts/TagsAndValues.cs
@@ -3,6 +3,26 @@ using System.Collections;
 
 public class TagsAndValues : MonoBehaviour {
 
+	private struct Tag {
+		public int id;
+		public string name;
+	}
+
+	private struct Value {
+		public int id;
+		public string name;
+		public int value;
+	}
+
+	private List<Tag> tags = new List<Tag>();
+
+	public void AddTag(int id, string name) {
+		new Tag tag = new Tag();
+		tag.id = id;
+		tag.name = name;
+		tags.Add(tag);
+	}
+	/*
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +32,5 @@ public class TagsAndValues : MonoBehaviour {
 	void Update () {
 	
 	}
+	*/
 }
---
+++ Assets/scripts/TagsValuesManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TagsValuesManager : MonoBehaviour {
+	/*
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	*/
+
+	private struct TagTemplate {
+		public int id;
+		public string name;
+		//public list of actions
+	}
+
+	private struct ValueTemplate {
+		public int id;
+		public string name;
+	}
+
+	private List<TagTemplate> tagsList = new List<TagTemplate>();
+	private List<ValueTemplate> valuesList = new List<ValueTemplate>();
+
+	public void NewTagTemplate(string tag) {
+		TagTemplate newTag = new TagTemplate();
+		newTag.id = tagsList.Count;
+		newTag.name = tag;
+		tagsList.Add(newTag);
+	}
+
+	public void NewValueTemplate(string value) {
+		ValueTemplate newValue = new ValueTemplate();
+		newValue.id = valuesList.Count;
+		newValue.name = value;
+		valuesList.Add(newValue);
+	}
+
+
+
+	void Start () {
+		NewTagTemplate("card"); //Default tags that exist
+	}
+	
+
+}
