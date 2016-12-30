30046ae6b0e1b80e560d5b353e38f3b68d250d27
Preetham N <preetham.in.1997@gmail.com>
2016-12-30 00:01:37 -0800

    Added and set up Photon Unity Networking to project

--- Assets/scripts/ItemController.cs
+++ Assets/scripts/ItemController.cs
@@ -7,23 +7,23 @@ using UnityEngine.UI;
 
 public class ItemController : MonoBehaviour {
     public Button sampleButton;                         // sample button prefab
-    private List<ContextMenuItem> contextMenuItems;     // list of items in menu
+    private List<ActionMenuItem> ActionMenuItems;     // list of items in menu
 
     public void CreateMenu(HashSet<TagsManager.Operation> operations, List<GameObject> gameObjects, int param) {
-        contextMenuItems = new List<ContextMenuItem>();
+        ActionMenuItems = new List<ActionMenuItem>();
         foreach (TagsManager.Operation operation in operations) {
             Func<List<GameObject>, int> func = operation.Func();
-            contextMenuItems.Add(new ContextMenuItem(operation.Label(), sampleButton, func));
+            ActionMenuItems.Add(new ActionMenuItem(operation.Label(), sampleButton, func));
         }
-        contextMenuItems.Add(new ContextMenuItem("Close", sampleButton, (List<GameObject> o) => {
+        ActionMenuItems.Add(new ActionMenuItem("Close", sampleButton, (List<GameObject> o) => {
             FindObjectOfType<CameraScript>().DestroyContentPanel();
             return 1;
         }));
         //Vector3 pos = Camera.main.WorldToScreenPoint(transform.position); // get screen point of current transform where you've rightclicked
-        ContextMenu.Instance.CreateContextMenu(contextMenuItems, new Vector2(0,0), gameObjects, param); //pass the gameobjects and any parameters you want to the context menu for actions
+        ActionMenu.Instance.CreateActionMenu(ActionMenuItems, new Vector2(0,0), gameObjects, param); //pass the gameobjects and any parameters you want to the context menu for actions
     }
 
     public void DestroyMenu() {
-        ContextMenu.Instance.DestroyContentPanel();
+        ActionMenu.Instance.DestroyContentPanel();
     }
 }
